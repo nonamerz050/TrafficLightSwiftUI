54d0241b43f0116187b45893770e0a2ac7b7b9e1
Nonamerz <a.g.kolesnik@yandex.ru>
2020-07-21 21:04:24 +0300

    Initial Commit

---
+++ TrafficLightSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TrafficLightSwiftUI
+//
+//  Created by MacBook Pro on 21.07.2020.
+//  Copyright © 2020 Artem K. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
